850b64d747f85b17af333ff00f7bf8fcdf1746a4
Keila Hoover <keila.hoover@gmail.com>
2018-04-03 11:30:47 -0600

    Add minidiscs migrations

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,12 @@
 module.exports = {
 development: {
  client: 'pg',
- connection: 'postgres://localhost/dbname-dev'
+ connection: 'postgres://localhost/minirack-dev'
 },
-test: {},
+test: {
+  client: 'pg',
+  connection: 'postgres://localhost/minirack-test'
+},
 production: {
  client: 'pg',
  connection: process.env.DATABASE_URL
